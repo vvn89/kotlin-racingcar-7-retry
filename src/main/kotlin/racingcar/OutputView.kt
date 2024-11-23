b1f8850051b8d8583af00b64cdce817d7c39f26e
지원 <cheerfulboys@naver.com>
2024-11-23 16:16:37 +0900

    feat(OutputView): 우승자 출력 기능 추가

--- src/main/kotlin/racingcar/OutputView.kt
+++ src/main/kotlin/racingcar/OutputView.kt
@@ -1,4 +1,8 @@
 package racingcar
 
 class OutputView {
+    fun printWinner(winners: List<String>) {
+        val finalWinner = winners.joinToString( ", " )
+        println("최종 우승자 : $finalWinner")
+    }
 }
