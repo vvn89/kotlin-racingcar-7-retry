28002edc44d32e63403b4ce47119d654890147bb
지원 <cheerfulboys@naver.com>
2024-11-23 18:12:54 +0900

    feat(RacingGame): 이름 검증, 시도 횟수 검증 기능 추가

--- src/main/kotlin/racingcar/RacingGame.kt
+++ src/main/kotlin/racingcar/RacingGame.kt
@@ -1,8 +1,51 @@
 package racingcar
 
 import camp.nextstep.edu.missionutils.Randoms
+import racingcar.view.InputView
+import racingcar.view.OutputView
 
 class RacingGame {
+    val inputView = InputView()
+    val outputView = OutputView()
+
+    fun run() {
+        val inputNames = inputView.readNames()
+        val inputNumber = inputView.readNumber()
+        val names = splitNames(inputNames)
+        val number = convertNumber(inputNumber)
+    }
+
+    private fun convertNumber(inputNumber: String): Int {
+        validateInputNumber(inputNumber)
+        return inputNumber.toInt()
+    }
+
+    fun createCars(names: String) {
+
+    }
+
+    private fun validateInputName(input: String) {
+        if (input.isEmpty()) {
+            throw IllegalArgumentException()
+        }
+    }
+
+    private fun validateInputNumber(input: String) {
+        if (input.isEmpty()) {
+            throw IllegalArgumentException()
+        }
+        try {
+            input.toInt()
+        } catch (e: Exception) {
+            throw IllegalArgumentException()
+        }
+    }
+
+    private fun splitNames(inputNames: String): List<String> {
+        validateInputName(inputNames)
+        return inputNames.split(",")
+    }
+
     fun generateRandomNumber() = Randoms.pickNumberInRange(RANDOM_NUMBER_RANGE_MIN, RANDOM_NUMBER_RANGE_MAX)
 
     fun isValidMoveCondition(number: Int): Boolean {
