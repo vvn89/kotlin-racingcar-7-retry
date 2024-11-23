2739e559830fb18e0f789b9b4a7820277ca95d49
지원 <cheerfulboys@naver.com>
2024-11-23 17:23:23 +0900

    feat(RacingGame): 랜덤 값 생성하는 기능 및 이동을 검증하는 기능 추가

--- src/main/kotlin/racingcar/RacingGame.kt
+++ src/main/kotlin/racingcar/RacingGame.kt
@@ -1,4 +1,20 @@
 package racingcar
 
+import camp.nextstep.edu.missionutils.Randoms
+
 class RacingGame {
+    fun generateRandomNumber() = Randoms.pickNumberInRange(RANDOM_NUMBER_RANGE_MIN, RANDOM_NUMBER_RANGE_MAX)
+
+    fun isValidMoveCondition(number: Int): Boolean {
+        if (number >= MINIMUM_MOVE_CONDITION) {
+            return true
+        }
+        return false
+    }
+
+    companion object {
+        const val RANDOM_NUMBER_RANGE_MIN = 0
+        const val RANDOM_NUMBER_RANGE_MAX = 9
+        const val MINIMUM_MOVE_CONDITION = 4
+    }
 }
