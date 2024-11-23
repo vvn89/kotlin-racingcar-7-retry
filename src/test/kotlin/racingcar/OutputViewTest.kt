1a18f50046dc1ce41dc030f0ae40dcee78e7b048
지원 <cheerfulboys@naver.com>
2024-11-23 19:18:34 +0900

    refactory(OutputView): 중복 코드 리팩토링

--- src/test/kotlin/racingcar/OutputViewTest.kt
+++ src/test/kotlin/racingcar/OutputViewTest.kt
@@ -2,15 +2,16 @@ package racingcar
 
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
+import racingcar.view.OutputView
 import java.io.ByteArrayOutputStream
 import java.io.PrintStream
 
 class OutputViewTest {
+    private val outputView = OutputView()
     @Test
     fun `최종 우승자 출력 1명 검증`() {
         //given
         val winners = listOf("pobi")
-        val outputView = OutputView()
         val outputStream = ByteArrayOutputStream()
         System.setOut(PrintStream(outputStream))
 
@@ -25,7 +26,6 @@ class OutputViewTest {
     fun `최종 우승자 출력 2명 검증`() {
         //given
         val winners = listOf("pobi", "java")
-        val outputView = OutputView()
         val outputStream = ByteArrayOutputStream()
         System.setOut(PrintStream(outputStream))
 
