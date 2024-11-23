ed5e41c47ebf94a664a401a7a363cc9ab031328c
지원 <cheerfulboys@naver.com>
2024-11-23 18:28:19 +0900

    feat(OutputView): 현황을 출력하는 기능 추가

--- src/main/kotlin/racingcar/view/OutputView.kt
+++ src/main/kotlin/racingcar/view/OutputView.kt
@@ -1,8 +1,18 @@
 package racingcar.view
 
+import racingcar.Car
+
 class OutputView {
+    fun printRacing(car: Car) {
+        println("${car.name} : ${DISTANCE_SYMBOL.repeat(car.distance)}")
+    }
+
     fun printWinner(winners: List<String>) {
         val finalWinner = winners.joinToString( ", " )
         println("최종 우승자 : $finalWinner")
+    }
+
+    companion object {
+        const val DISTANCE_SYMBOL = "-"
     }
 }
